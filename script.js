66a3f8491158cf4e3d24e62e606d0162d9508e6d
Alex Yang <jyan8933@uni.sydney.edu.au>
2025-03-18 22:21:41 +1100

    Add logic for operator buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const display = document.querySelector(".display");
 const clearButton = document.querySelector(".clear");
 const equalsButton = document.querySelector(".equals");
+const operatorButtons = document.querySelectorAll(".operator");
 const digitButtons = document.querySelectorAll(".digit");
 
 function add(a, b) {
@@ -42,6 +43,14 @@ clearButton.addEventListener("click", () => {
 })
 
 equalsButton.addEventListener("click", evaluate);
+
+for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener("click", (e) => {
+        if (display.textContent.length !== 0 && display.textContent.slice(-1) !== ' ') {
+            display.textContent += ` ${e.target.closest("button").textContent} `;
+        } 
+    })
+}
 
 for (let i = 0; i < digitButtons.length; i++) {
     digitButtons[i].addEventListener("click", (e) => {
