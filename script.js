fe6025178d83b808027870f227d443bf4cf4bcb6
Alex Yang <jyan8933@uni.sydney.edu.au>
2025-03-18 19:41:50 +1100

    Create operator function in JS

--- script.js
+++ script.js
@@ -16,4 +16,16 @@ function divide(a, b) {
     return a / b;
 }
 
-
+function operator(a, b, op) {
+    if (op === "+") {
+        return add(a, b);
+    } else if (op === "-") {
+        return subtract(a, b);
+    } else if (op === "x") {
+        return multiply(a, b);
+    } else if (op === "/") {
+        return divide(a, b);
+    } else {
+        return "INVALID OPERATOR";
+    }
+}
