4556d5491cc0d9251fd187c9f74da78c0693267e
Alex Yang <jyan8933@uni.sydney.edu.au>
2025-03-18 23:15:37 +1100

    Implement equals button logic

--- script.js
+++ script.js
@@ -30,11 +30,48 @@ function operator(a, b, op) {
     } else if (op === "/") {
         return divide(a, b);
     } else {
-        return "INVALID OPERATOR";
+        display.textContent = "INVALID OPERATOR";
+        return;
     }
 }
 
 function evaluate() {
+    // If display is empty or has hanging operator, do nothing
+    if (display.textContent.length === 0 || display.textContent.slice(-1) === " ") {
+        return;
+    }
+
+    // Split display into array of form ["a", "<operator>", "b", ...]
+    let expr = display.textContent.split(" ");
+
+    while (expr.length > 1) {
+        // Evaluate display expression by BODMAS
+        if (expr.includes("/")) {
+            // Find index of operator then use to find a, b to evaluate expression
+            const opIdx = expr.indexOf("/");
+            const result = operator(Number(expr[opIdx - 1]), Number(expr[opIdx + 1]), "/");
+            // Then replace ["a", "<operator>", "c"] with ["<result>"]
+            expr.splice(opIdx - 1, 3, result);
+        } else if (expr.includes("x")) {
+            const opIdx = expr.indexOf("x");
+            const result = operator(Number(expr[opIdx - 1]), Number(expr[opIdx + 1]), "x");
+            expr.splice(opIdx - 1, 3, result);
+        } else if (expr.includes("+")) {
+            const opIdx = expr.indexOf("+");
+            const result = operator(Number(expr[opIdx - 1]), Number(expr[opIdx + 1]), "+");
+            expr.splice(opIdx - 1, 3, result);
+        } else if (expr.includes("-")) {
+            const opIdx = expr.indexOf("-");
+            const result = operator(Number(expr[opIdx - 1]), Number(expr[opIdx + 1]), "-");
+            expr.splice(opIdx - 1, 3, result);
+        } else {
+            display.textContent("ERROR");
+            return;
+        }
+    }
+
+    // Display result rounded to 2 d.p
+    display.textContent = Math.round(expr[0] * 100) / 100;    
 
 }
 
@@ -46,7 +83,8 @@ equalsButton.addEventListener("click", evaluate);
 
 for (let i = 0; i < operatorButtons.length; i++) {
     operatorButtons[i].addEventListener("click", (e) => {
-        if (display.textContent.length !== 0 && display.textContent.slice(-1) !== ' ') {
+        // Only add operator to display if there isn't already an operator added
+        if (display.textContent.length !== 0 && display.textContent.slice(-1) !== " ") {
             display.textContent += ` ${e.target.closest("button").textContent} `;
         } 
     })
